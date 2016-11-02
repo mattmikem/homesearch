ea4d1284bdac3cc36d6f534a231dbb80e181dd31
Matthew Miller <matthewmiller@Matthews-MacBook-Pro.local>
2016-11-02 11:44:08 -0700

    Camera alignment updates

--- HS.py
+++ HS.py
@@ -4,10 +4,14 @@
 
 #Following borrowed from: https://andrewpwheeler.wordpress.com/2015/12/28/using-python-to-grab-google-street-view-imagery/
 
+#example url:https://maps.googleapis.com/maps/api/streetview?size=600x300&location=46.414382,10.013988&heading=151.78&pitch=-0.76&key=YOUR_API_KEY
+
+
 #Key 1: 
 key1 = "AIzaSyCr9tXJARt5KW7nUC3B03lHip_9P9Z0F44"
 
 import urllib, os
+import sys
 
 myloc = r"/Users/matthewmiller/Dropbox/Research/Urban/Projects/HomeSearch/Images" #replace with your own location
 key  = "&key=" + key1 #got banned after ~100 requests with no key
@@ -34,27 +38,37 @@ print add_list[1]
 
 add_tuple = []
 add_feed  = []
+head_feed = []
 
 
 aa = 0
 
 for a in add_list:
     add_tuple = add_tuple + [tuple(a.split(','))]
+    #This allows the (x,y) coords or address location
+    #atemp = str(add_tuple[aa][1])+","+str(add_tuple[aa][2])+"&heading="+str(add_tuple[aa][6])
+    #add_feed = add_feed + [atemp]
+    #head_feed = head_feed + [add_tuple[aa][6]]
     atemp = str(add_tuple[aa][6]) + " " +add_tuple[aa][3]+", "+add_tuple[aa][4]+", CA"+" "+str(add_tuple[aa][5])
-    add_feed = add_feed + [atemp.replace('"','')]
+    htemp = "&heading="+str(add_tuple[aa][7])
+    add_feed = add_feed + [atemp.replace('"','')+htemp]
     aa += 1
 
+#print add_feed[1:5]
+#sys.exit(0)
+    
 #Use address list into GSV API
     
 def GetStreet(Add,SaveLoc):
   base = "https://maps.googleapis.com/maps/api/streetview?size=1200x800&location="
-  #face = "&heading="+str(Head)
+  #face = "&heading="+str(head)
   MyUrl = base + Add + key
   fi = Add + ".jpg"
   urllib.urlretrieve(MyUrl, os.path.join(SaveLoc,fi))
 
 #Numbers for range of street addresses on 6th street from Main St to LA River
           
+#jj = 0    
 for j in add_feed:
     GetStreet(Add=j,SaveLoc=myloc)
     
--- HS_parse.R
+++ HS_parse.R
@@ -1,17 +1,44 @@
 #HS_parse.R
-#Parse through OA (OpenAddresses) alongside .py module
+#Matt Miller, Oct 2016
+
+#Parse through OA (OpenAddresses) alongside HS.py module, map locations
 
 #install.packages('reshape2')
 #install.packages('ggmap')
 #install.packages('ggplot2')
 #install.packages('doBy')
 #install.packages('taRifx')
+#install.packages('geosphere')
+#install.packages('fields')
+#install.packages('e1071')
 
 library(reshape2)
 library(ggmap)
 library(ggplot2)
 library(doBy)
 library(taRifx)
+library(geosphere)
+library(fields)
+library(e1071)
+
+#Theta function, keep consistent camera alignment (changes 'heading' in GSV API)
+
+#Current method uses heading aligned with orthongonal vector to vector between an address and its
+#nearest neighbor, and is subject to small pertubations (still, it does rather well).
+#Options less sensitive: moving, within street odd/even-address linear regression, or moving
+#within street SVM (approximating road between odd and even addresses).
+
+#Note - addresses have (lon,lat) coordinates, so every address is a 2x1 vector
+
+norm <- function(x) sqrt(sum(x^2))
+
+theta <- function(add0, add1) {
+  add_vector  <- add1 - add0
+  orth_vector <-  c(-1*add_vector[2], add_vector[1])
+  north_vector <- c(orth_vector[1],0)
+  theta <- 180*acos((north_vector %*% orth_vector)/(norm(orth_vector)*norm(north_vector)))/pi
+  return(theta)
+}
 
 #Test on LA addresses
 
@@ -24,14 +51,46 @@ la_add <- read.csv(paste(ca_path, "los_angeles.csv", sep="/"), header=TRUE, sep=
 num_split <- colsplit(la_add$NUMBER, " ", c('NUM1','NUM2'))
 la_add <- cbind(la_add, num_split)
 la_add$NUM1 <- destring(la_add$NUM1)
+la_add$ODD <- la_add$NUM1 %% 2
 
 #Limit within four LA streets
 
-streets <- c('East 6th Street', 'East 3rd Street', 'South Central Avenue', 'South Spring Street')
+#streets <- c('East 6th Street', 'East 3rd Street', 'South Central Avenue', 'South Spring Street')
+streets <- c('East 4th Street')
 
-la_test <- la_add[la_add$STREET %in% streets & la_add$CITY == 'Los Angeles' & la_add$NUM1<1400 & la_add$NUM2 == "",]
+la_test <- la_add[la_add$STREET %in% streets & la_add$CITY == 'Los Angeles' & la_add$NUM1<1400 & la_add$NUM2 == "" & la_add$ODD == 1,]
 
-##Map Addreses selected
+#Flag closest address on same street side
+
+la_test <- la_test[order(la_test$ODD, la_test$STREET, la_test$NUM1),]
+
+la_test$LAG  <- rep(0,nrow(la_test))
+la_test$LEAD <- rep(0,nrow(la_test))
+la_test$LOOK <- rep(0,nrow(la_test))
+la_test$THETA <- rep(0,nrow(la_test))
+
+
+for(i in 2:(nrow(la_test)-1)) {
+  la_test$LAG[i]  <- dist(t(cbind(c(la_test$LAT[i],la_test$LON[i]),c(la_test$LAT[i-1],la_test$LON[i-1]))), method = 'euclidean')
+  la_test$LEAD[i] <- dist(t(cbind(c(la_test$LAT[i],la_test$LON[i]),c(la_test$LAT[i+1],la_test$LON[i+1]))), method = 'euclidean')
+  if (la_test$LEAD[i] > la_test$LAG[i] & la_test$STREET[i] == la_test$STREET[i-1]) {
+    la_test$LOOK[i] <- -1
+  }
+  else if (la_test$LEAD[i] < la_test$LAG[i] & la_test$STREET[i]==la_test$STREET[i+1]) {
+    la_test$LOOK[i] <- 1  
+  }
+  look <- i + la_test$LOOK[i]
+  la_test$THETA[i] <- round(theta(c(la_test$LAT[i],la_test$LON[i]),c(la_test$LAT[look],la_test$LON[look])))
+}
+
+##To keep
+
+la_test <- la_test[,c("LAT", "LON", "STREET", "CITY", "POSTCODE", "NUM1", "THETA")]
+la_test <- la_test[order(la_test$STREET, la_test$NUM1),]
+
+write.csv(la_test, 'la_test.csv')
+
+##Map Addresses selected
 
 #attach(la_test)
 
@@ -51,9 +110,3 @@ city_map <- qmap(c_point, zoom = 14)
 
 city_map + geom_point(aes(x = la_test$LON, y = la_test$LAT, color = la_test$STREET), data = la_test)
 
-##To keep
-
-la_test <- la_test[,c("LAT", "LON", "STREET", "CITY", "POSTCODE", "NUM1")]
-la_test <- la_test[order(la_test$STREET, la_test$NUM1),]
-
-write.csv(la_test, 'la_test.csv')
